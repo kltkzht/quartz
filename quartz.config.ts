fc18ce439691b5bbbe310ee8184475f735ea43fd
kltkzht <trifonova.kate.s@gmail.com>
2026-02-12 22:54:14 +0100

    Update quartz.config.ts
    eth-notes to quartz

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "kltkzht.github.io/eth-notes",
+    baseUrl: "kltkzht.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
